b8266f60401cc828015b60f3ce23d942ff6196c1
geniushee <h1116jeon@gmail.com>
2023-10-30 14:10:36 +0900

    다수의 연산 기능 추가 but error

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -4,31 +4,91 @@ import lombok.Getter;
 
 public class Calc {
     @Getter
-    public int Res;
-//
-//    Calc() {
-//        Res = 0;
-//    }
-//
-    public static int run(String text) {
-//        List<String> list;
-//
-//        list = new ArrayList<>();
-//        int j = 0;
-////        String[] temp = new String[text.length()];
-//        for (int i = 1; i <= text.length(); i++) {
-//            String t = text.substring(i - 1, i);
-//            if (t.equals(" ")) ;
-//            else {
-//                list.add(t);
-//                j++;
-//            }
-//        }
-//        int index = list.indexOf("*");
-//        if (index != -1) {
-//            Res = Integer.parseInt(list.get(index - 1)) * Integer.parseInt(list.get(index + 1));
-//        }
-//
-        return 20;
+    private int Res;
+    private String Operate;
+
+    Calc() {
+        Res = 0;
+        Operate = "+";
+    }
+
+    public int run(String exp) {
+        if (exp.isBlank()) return 0;
+
+        if (operateCheck(exp)) {
+            String[] Sarray = operateSplit(exp);
+            int[] intarray = StringAsInt(Sarray);
+
+            switch (Operate) {
+                case "\\+":
+                    Res = plus(intarray);
+                    break;
+                case "-":
+                    Res = minus(intarray);
+                    break;
+                case "\\*":
+                    Res = multiple(intarray);
+                    break;
+                case "/":
+                    Res = divide(intarray);
+                    break;
+            }
+        }
+
+
+        return Res;
+    }
+
+    Boolean operateCheck(String exp) {
+        return exp.contains("+") || exp.contains("-") || exp.contains("*") || exp.contains("/");
+    }
+
+    String[] operateSplit(String exp) {
+        if (exp.contains("+")) { //곱하기 분기
+            Operate = "\\+";
+        } else if (exp.contains("-")) { //더하기 분기
+            Operate = "-";
+        } else if (exp.contains("*")) {
+            Operate = "\\*";
+        } else if (exp.contains("/")) {
+            Operate = "/";
+        }
+
+
+        String[] Sarray = exp.split(Operate);
+        for(int i = 0;i<Sarray.length;i++){
+            Sarray[i] = Sarray[i].trim();
+        }
+        return Sarray;
+    }
+
+    int[] StringAsInt(String[] Sarray) {
+        for (int i = 0; i < Sarray.length; i++) {
+            if (operateCheck(Sarray[i])) {
+                Sarray[i] = String.valueOf(run(Sarray[i]));
+            }
+        }
+
+        int[] intarray = new int[2];
+        intarray[0] = Integer.parseInt(Sarray[0].trim());
+        intarray[1] = Integer.parseInt(Sarray[1].trim());
+
+        return intarray;
+    }
+
+    private int multiple(int[] intarray) {
+        return intarray[0] * intarray[1];
+    }
+
+    int plus(int[] intarray) {
+        return intarray[0] + intarray[1];
+    }
+
+    int minus(int[] intarray) {
+        return intarray[0] - intarray[1];
+    }
+
+    int divide(int[] intarray) {
+        return intarray[0] / intarray[1];
     }
 }
--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -7,6 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class CalcTest {
+    Calc Calc = new Calc();
     @Test
     @DisplayName("Calc의 run메소드는 int를 리턴한다.")
     void t1(){
@@ -18,5 +19,40 @@ public class CalcTest {
     void t2(){
         int rs = Calc.run("10 + 10");
         assertThat(rs).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("10 + 20 = 30")
+    void t3() {
+        int rs = Calc.run("10 + 20");
+        assertThat(rs).isEqualTo(30);
+    }
+
+    @Test
+    @DisplayName("10 * 30 = 300")
+    void t4() {
+        int rs = Calc.run("10 * 30");
+        assertThat(rs).isEqualTo(300);
+    }
+
+    @Test
+    @DisplayName("30 - 10 = 310")
+    void t5() {
+        int rs = Calc.run("30 - 10");
+        assertThat(rs).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("30 / 10 = 3")
+    void t6() {
+        int rs = Calc.run("30 / 10");
+        assertThat(rs).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("10 + 30 * 10  = 3")
+    void t7() {
+        int rs = Calc.run("10 + 30 / 10");
+        assertThat(rs).isEqualTo(13);
     }
 }

19318876d4a0747f5a43d1c0caf39916b7ed473b
geniushee <h1116jeon@gmail.com>
2023-10-30 10:51:09 +0900

    TDD방식 코딩 시작  assertThat 임포트 및 설정

---
+++ src/main/java/com/ll/Calc.java
@@ -0,0 +1,34 @@
+package com.ll;
+
+import lombok.Getter;
+
+public class Calc {
+    @Getter
+    public int Res;
+//
+//    Calc() {
+//        Res = 0;
+//    }
+//
+    public static int run(String text) {
+//        List<String> list;
+//
+//        list = new ArrayList<>();
+//        int j = 0;
+////        String[] temp = new String[text.length()];
+//        for (int i = 1; i <= text.length(); i++) {
+//            String t = text.substring(i - 1, i);
+//            if (t.equals(" ")) ;
+//            else {
+//                list.add(t);
+//                j++;
+//            }
+//        }
+//        int index = list.indexOf("*");
+//        if (index != -1) {
+//            Res = Integer.parseInt(list.get(index - 1)) * Integer.parseInt(list.get(index + 1));
+//        }
+//
+        return 20;
+    }
+}
--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -2,6 +2,12 @@ package com.ll;
 
 public class Main {
     public static void main(String[] args) {
+        Calc Calc;
+        String text;
 
+        Calc = new Calc();
+        text = "3 * 1";
+        Calc.run(text);
+        System.out.println(text + " = " + Calc.getRes());
     }
 }
